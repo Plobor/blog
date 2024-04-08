22ab5319bf758535a3b42ba59b7fed0d854ed583
Aprendible <jorge@aprendible.com>
2024-04-08 10:35:51 -0500

    10 - Plantillas de blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,3 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nosotros</title>
-</head>
-<body>
-@include('partials.navigation')
-<h1>Nosotros</h1>
-</body>
-</html>
+<x-layout meta-title="About title" meta-description="About description">
+    <h1>About</h1>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,3 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-</head>
-<body>
-@include('partials.navigation')
-<h1>Inicio</h1>
-</body>
-</html>
-
+<x-layout meta-title="Home title" meta-description="Home description">
+    <h1>Inicio</h1>
+</x-layout>
