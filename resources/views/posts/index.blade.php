4420d849abfb7330a3cfc1daa70caa17c55a2742
Aprendible <jorge@aprendible.com>
2024-04-16 12:06:43 -0500

    15 - Eloquent: find by id

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 <x-layout meta-title="Blog title" meta-description="Blog description">
     <h1>Blog</h1>
     @foreach ($posts as $post)
-        <h2>{{ $post->title }}</h2>
+        <h2>
+            <a href="{{ route('posts.show', $post) }}">
+                {{ $post->title }}
+            </a>
+        </h2>
     @endforeach
 </x-layout>
