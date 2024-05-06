82884a7fb651a83b82b8bef8ecce17204701b348
Aprendible <jorge@aprendible.com>
2024-05-05 21:59:55 -0500

    21 - Edit Form

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,28 @@
+<x-layout :meta-title="$post->title" :meta-description="$post->body">
+    <h1>Edit form</h1>
+
+    <form action="{{ route('posts.update', $post) }}" method="POST">
+        @csrf @method('PATCH')
+        <label>
+            {{ __('Title') }} <br />
+            <input name="title" type="text" value="{{ old('title', $post->title) }}"/>
+            @error('title')
+            <br />
+            <small style="color: red">{{ $message }}</small>
+            @enderror
+        </label> <br />
+        <label>
+            {{ __('Body') }} <br />
+            <textarea name="body">{{old('body', $post->body)}}</textarea>
+            @error('body')
+            <br />
+            <small style="color: red">{{ $message }}</small>
+            @enderror
+        </label><br />
+        <button type="submit">{{ __('Send') }}</button>
+        <br />
+    </form>
+    <br />
+
+    <a href="{{ route('posts.index') }}">Back</a>
+</x-layout>
