f6fcab0ed22ed9f32cd0ea39d8257a408d06ed9f
Aprendible <55994087+aprendible@users.noreply.github.com>
2024-06-28 21:41:29 -0500

    28 - Tailwind Template

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,7 @@
 <x-layout meta-title="Contact title" meta-description="Contact description">
-    <h1>Contact</h1>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Contact
+        </h1>
+    </div>
 </x-layout>
