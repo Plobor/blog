738af1b335b29d053fe5f705b53d36202f15ea37
Aprendible <jorge@aprendible.com>
2024-06-30 18:22:05 -0500

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -45,13 +45,3 @@
         </div>
     </article>
 </x-layout>
-<div style="display: flex; align-items: baseline">
-    <h2>
-        <a href="{{ route('posts.show', $post) }}"> {{ $post->title }} </a>
-    </h2>
-    &nbsp; <a href="{{ route('posts.edit', $post) }}">Edit</a> &nbsp;
-    <form action="{{ route('posts.destroy', $post) }}" method="POST">
-        @csrf @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
-</div>
