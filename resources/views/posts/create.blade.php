4eea1ca6c7ea3bb63086b4871d372c8b3b8834c1
Aprendible <jorge@aprendible.com>
2024-07-08 11:40:25 -0500

    31 - Admin Panel - Parte 2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,11 +11,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="{{ route('posts.store') }}" method="POST">
-                        @csrf
+                    <form action="{{ route('posts.store') }}"
+                        method="POST"
+                        class="space-y-4 max-w-xl"
+                    >
                         @include('posts.form-fields')
-                        <button type="submit">{{ __('Send') }}</button>
-                        <br />
+                        <x-primary-button type="submit">{{ __('Save') }}</x-primary-button>
+                        @csrf
                     </form>
                 </div>
             </div>
